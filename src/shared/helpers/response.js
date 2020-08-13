c707b73ab421d2c2892ad485e29e55ee6c65695f
An <habinhan1999@gmail.com>
2020-08-13 14:13:48 +0700

    response total at the end

--- src/shared/helpers/response.js
+++ src/shared/helpers/response.js
@@ -8,7 +8,7 @@ export function responseSuccess(res, data, status) {
     console.log(
       chalk.greenBright(
         util.inspect(
-          { Total: _.defaultTo(data.length, 1), data },
+          { data, Total: _.defaultTo(data.length, 1) },
           { showHidden: false, depth: null },
         ),
       ),
